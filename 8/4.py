6ea9695db62708e0118028dfe98cc75520c2590f
norgor <7034394+norgor@users.noreply.github.com>
2022-10-15 21:33:55 +0200

    fourpy

--- 8/4.py
+++ 8/4.py
@@ -9,8 +9,8 @@ env = gym.make("CartPole-v1")
 
 OBSERVATION_SPACE_N = env.observation_space.shape[0]
 ACTION_SPACE_N = env.action_space.n
-DISCRETIZE_N = 16385
-DISCRETIZED_INDEX_OFFSET = np.arange(0, OBSERVATION_SPACE_N, 1) * DISCRETIZE_N
+DISCRETIZE_N = 64
+MAX_REWARD = 500
 
 n_bins = (DISCRETIZE_N, ) * OBSERVATION_SPACE_N
 
@@ -24,48 +24,51 @@ def discretize(observations):
     ]
 
     est.fit(X)
-    T = est.transform([observations])
-    return tuple(map(int, T[0]))
+    T = est.transform([observations])[0]
+
+    state = 0
+    mult = 1
+    for i in T:
+        state += int(i) * mult
+        mult *= DISCRETIZE_N
+
+    return state
 
 
-def discretize_idx(observations):
-    return discretize(observations) + DISCRETIZED_INDEX_OFFSET
+Q_table = np.zeros((DISCRETIZE_N**OBSERVATION_SPACE_N, ACTION_SPACE_N))
 
-
-Q_table = np.zeros((OBSERVATION_SPACE_N * DISCRETIZE_N, ACTION_SPACE_N))
-
-ALPHA = 1  # [0,1]
-GAMMA = 1  # [0,1]
+ALPHA = 0.1  # [0,1]
+GAMMA = 0.4  # [0,1]
 
 EPISODES = 100_000
 scores = np.zeros(EPISODES)
 for e in tqdm(range(EPISODES)):
     # Discretize state into buckets
     obs = env.reset()[0]
-    state = discretize_idx(obs)
+    state = discretize(obs)
 
+    # epsilon greedy strategy
     epsilon = max(.01, min(1., 1. - np.log10((e + 1) / 25)))
     terminated = False
     truncated = False
     reward = 0
 
-    while not terminated or truncated:
+    while not terminated and not truncated:
         # policy action
-        action = np.argmax(np.max(Q_table[state, :], axis=0))
+        action = np.argmax(Q_table[state])
 
         # insert random action
-        # epsilon greedy strategy
         if np.random.random() < epsilon:
             action = env.action_space.sample()  # explore
 
         # increment enviroment
         obs, _, terminated, truncated, _ = env.step(action)
-        new_state = discretize_idx(obs)
+        new_state = discretize(obs)
 
         # Update Q-Table
-        Q_table[state, action] += epsilon*ALPHA*(
+        Q_table[state, action] += ALPHA*(
             reward +
-            epsilon*GAMMA*np.max(Q_table[new_state, :]) -
+            GAMMA*np.max(Q_table[new_state, :]) -
             Q_table[state, action]
         )
 
@@ -76,5 +79,5 @@ for e in tqdm(range(EPISODES)):
 
 print(scores)
 
-plt.plot(scores,  c='blue', label='epochs')
+plt.plot(scores / MAX_REWARD,  c='blue', label='epochs')
 plt.savefig("plot.png")
